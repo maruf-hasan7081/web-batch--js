24871127548bda25ce4e74395be27145ba9ad670
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-02-25 05:15:02 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,56 @@
+// console.log("12");
+
+// console.log("my name is akbor");
+
+
+// var num = 5;
+// num=10;
+// console.log(num);
+
+
+// var num =20;
+// num=50;
+// console.log(num);
+
+
+// var name = "chadni";
+// console.log(name);
+
+
+// var name="xyz";
+// console.log(name);
+
+
+/**
+ * variable name likhar niam
+ *  var var likhte parbo na 
+ * var my name= space dite parbo na
+ *  var "name" = quote er bithore variable name likhte parbo na
+ * 
+ * var 1name = number diye shuru korte parbo na 
+ * 
+ * variable name hocche case sensitive:=-------- var name= "maruf"  ... var Name ="maruf" 
+ * 
+ * var likhte parbo -----Var likhte parbo na ------ VAR likhte parbo na
+ * 
+ * 
+ * var my_name_is="xyz"-----------------snake case
+ * 
+ * var myNameIs="xyz"-------------------camel case
+ * 
+ * var MyName="xyz"-------------------pascal case
+ * 
+ * 
+ * //
+ 
+
+
+
+//  var isnum=ture;
+
+
+
+
+
+
+var isnum=ture;
---
+++ second.js
@@ -0,0 +1,49 @@
+// var name ="ture"
+// console.log(typeof name);
+
+
+// var isnum=true;
+// console.log(typeof isnum);
+
+
+
+// var num =10;
+// console.log(typeof num);
+
+
+
+
+// /// type casting
+
+
+// var num2= 10.5;
+// // console.log(typeof num2);
+// console.log(parseInt(num2));
+
+
+// var num3 ='20'
+// // console.log(typeof num3);
+// console.log (typeof parseInt(num3));
+// // console.log(typeof num3);
+
+
+
+var num4 = 20;
+console.log(parseFloat(num4));
+
+
+
+
+
+
+var num5 ="40";
+var num6 =20;
+
+var result=num5+num6;
+console.log(result);
+
+
+
+
+
+
