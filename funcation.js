7bd1356aca284d7e67b824a821ca83e8602d51dd
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-02-27 23:46:19 +0600

    add

---
+++ funcation.js
@@ -0,0 +1,13 @@
+// function borovaiercomman(){
+
+
+//     console.log("tui amke 10 taka de ")
+
+// }
+// borovaiercomman();
+
+
+function add ( a , b){
+    console.log(a+b);
+}
+add(10 , 20);
---
+++ loop.js
@@ -0,0 +1,65 @@
+// // for of loop
+
+
+// let number =[10,20,30,40,50];
+
+// for(let num of number){
+//     console.log(num);
+// }
+
+
+// // for loop
+
+// // for(first parant (loop variable )   ; second parant (condtition )     ; third parant (increment / decrement)     ){
+// //     //code excute 
+// // }
+
+
+
+// for ( let i = 2; i<=10 ; i+=2 ){
+//     console.log(i);
+// }
+
+
+// for ( let i = 10; i>=0 ; i-- ){
+//     console.log(i);
+// }
+
+
+
+// while loop
+
+
+
+// (variable )
+// while(condition){
+//     //code excute
+//     (increment/ decrement)
+// }
+
+
+
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+let number =1;
+while(number<10){
+    if(number%2!=0){
+        console.log(number);
+    }
+    number++;
+
+
+}
+
+
+
+
+
+
+
+
+
